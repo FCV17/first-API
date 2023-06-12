5c51c97f6e50ad94eaf8548678a1d5ef2d4336bf
filipeveiga@FCVMac <filipe.veiga@protonmail.com>
2023-06-12 15:12:01 +0100

    ,added body-parser and some code to server.js

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var ingredients [
+	{
+		"id":"5726364",
+		"text": "eggs"	
+	},
+	{   "id":"385955",
+		"text": "milk"
+	},
+	{	"id":"43535",
+		"text": "bacon"
+	},
+	{	
+		"id":"9835354",
+		"text": "frog legs"
+	}
+];
 
 app.get('/', function (req, res){
-	res.send('My first API!');
+	res.send(ingredients);
 });
 
 
